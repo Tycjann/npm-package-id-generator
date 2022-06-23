f3572f7348b708deebff7248ce4484f8d803ac3b
Mirosław Gucwa <mgucwa@gmail.com>
2022-06-23 08:51:41 +0200

    Change char list + specjal chars

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unique ID generator
+**Character set:** ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?
+* **Info:** You can specify the length of the ID
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const randomID = (idLength) => {
   let id = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?';
   const charsAmount = characters.length;
   for (let i = 0; i < idLength; i++) {
     id += characters.charAt(Math.floor(Math.random() * charsAmount));
